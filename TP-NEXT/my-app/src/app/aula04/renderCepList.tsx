5bd087f4cec69480d896c7af26536ec87e59523c
Augusto Dantas <augustodantas.dev@gmail.com>
2024-11-08 19:17:15 -0300

    feat: atualizacao do botao de renderizar lista

--- TP-NEXT/my-app/src/app/aula04/renderCepList.tsx
+++ TP-NEXT/my-app/src/app/aula04/renderCepList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 
 type DataCep = {
 	cep: string;
@@ -14,33 +14,39 @@ type RenderCepListProps = {
 
 const RenderCepList: React.FC<RenderCepListProps> = ({ cepDataList }) => {
 	const [showList, setShowList] = useState(false);
+	const [displayList, setDisplayList] = useState<DataCep[]>([]);
+
+	const handleShowList = () => {
+		setDisplayList([...cepDataList]); 
+		setShowList(true); 
+	};
 
 	return (
 		<div>
 			<button
 				className="bg-green-500 text-white font-medium rounded px-4 py-2"
-				onClick={() => setShowList(!showList)}
+				onClick={handleShowList}
 			>
-				{showList ? "Ocultar Lista de CEPs" : "Exibir Lista de CEPs"}
+				Exibir Lista de CEPs
 			</button>
 
-			{showList && cepDataList.length > 0 && (
+			{showList && (
 				<div className="mt-4">
 					<h3 className="text-xl font-semibold mb-2">Todos os CEPs Pesquisados:</h3>
-					{cepDataList.map((cep, index) => (
-						<div key={index} className="bg-gray-100 w-[450px] p-4 rounded mb-2">
-							<p><strong>CEP:</strong> {cep.cep}</p>
-							<p><strong>Logradouro:</strong> {cep.logradouro}</p>
-							<p><strong>Bairro:</strong> {cep.bairro}</p>
-							<p><strong>Localidade:</strong> {cep.localidade}</p>
-							<p><strong>UF:</strong> {cep.uf}</p>
-						</div>
-					))}
+					{displayList.length > 0 ? (
+						displayList.map((cep, index) => (
+							<div key={index} className="bg-gray-100 w-[450px] p-4 rounded mb-2">
+								<p><strong>CEP:</strong> {cep.cep}</p>
+								<p><strong>Logradouro:</strong> {cep.logradouro}</p>
+								<p><strong>Bairro:</strong> {cep.bairro}</p>
+								<p><strong>Localidade:</strong> {cep.localidade}</p>
+								<p><strong>UF:</strong> {cep.uf}</p>
+							</div>
+						))
+					) : (
+						<p className="text-gray-500 mt-4">Nenhum CEP armazenado.</p>
+					)}
 				</div>
-			)}
-
-			{showList && cepDataList.length === 0 && (
-				<p className="text-gray-500 mt-4">Nenhum CEP armazenado.</p>
 			)}
 		</div>
 	);
